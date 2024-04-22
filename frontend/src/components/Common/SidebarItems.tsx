7854ec89aee959a5473d22563f335e1eab7dcadf
Carlos García Ling <carlos.gling@hotmail.com>
2024-04-22 22:32:14 +0200

    Change icon for sensors

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -2,13 +2,14 @@ import { Box, Flex, Icon, Text, useColorModeValue } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link } from "@tanstack/react-router"
 import { FiBriefcase, FiHome, FiSettings, FiUsers } from "react-icons/fi"
+import { MdSensors } from "react-icons/md";
 
 import type { UserPublic } from "../../client"
 
 const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
   { icon: FiBriefcase, title: "Items", path: "/items" },
-  { icon: FiBriefcase, title: "Sensors", path: "/sensors" },
+  { icon: MdSensors, title: "Sensors", path: "/sensors" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
 ]
 
